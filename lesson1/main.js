a37fd5583207a21a25cc3992de64f0acaddd09e3
olehbin <lewonator@gmail.com>
2023-02-01 00:41:24 +0200

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,30 @@
+let ass = 'hello';
+let bass = ' owu';
+let cass = ' com ';
+let d = 'ua ';
+let e =  1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = !!1;
+let m = !!0;
+console.log(ass + bass + cass + d + e + " " + f + " " + g + " " + h + " " + i + " " + j + " " + k + " " + l + " " + m)
+
+let firstname = 'Viktoria ';
+let middlename = 'Myhavkiv ';
+let lastname = 'Yurivna';
+let person = firstname + middlename + lastname;
+console.log(person)
+ let a = typeof 100;
+ let b = typeof '100';
+ let c = typeof !!NaN;
+ console.log(a)
+ console.log(b)
+ console.log(c)
+
+
+
+
